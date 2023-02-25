1dcbc585172b4908c79ff6be218fce90a62c95b1
Aymen Jemi <jemiaymen@gmail.com>
2023-02-25 18:36:18 +0100

    add contact form jemix.tn

--- api.py
+++ api.py
@@ -1,8 +1,11 @@
 from sudoai.pipeline import Pipeline
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from urllib.parse import unquote
 from pysondb import db
+
+
+
 
 
 app = Flask(__name__)
@@ -48,7 +51,32 @@ class Sudoai(Resource):
         return analyse_dialect_tunisian(unquote(text)),  201, {'Access-Control-Allow-Origin': '*'}
 
 
+class ContactJemix(Resource):
+
+    def post(self):
+        
+        database = db.getDb("contact.json")
+        parser = reqparse.RequestParser()
+        parser.add_argument('name')
+        parser.add_argument('email')
+        parser.add_argument('msg_subject')
+        parser.add_argument('phone_number')
+        parser.add_argument('message')
+        args = parser.parse_args()
+
+        data = {"name" : args['name'] , 
+                "email" : args['email'], 
+                "subject" : args['msg_subject'], 
+                "tel" : args['phone_number'], 
+                "msg" : args['message']
+            }
+        
+        database.add(data)
+        return "success" , 201, {'Access-Control-Allow-Origin': '*'}
+    
+
 api.add_resource(Sudoai, '/<text>')
+api.add_resource(ContactJemix , '/jmx/contact')
   
 if __name__ == '__main__':
     app.run(debug = True)
